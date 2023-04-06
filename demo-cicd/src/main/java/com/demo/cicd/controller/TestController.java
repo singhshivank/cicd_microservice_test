254be41baef56dfcaca8c240ff6316cb95c1add2
shivank <singh@mch.nl>
2023-04-06 13:07:07 +0200

    fixed container port issue isuue

--- demo-cicd/src/main/java/com/demo/cicd/controller/TestController.java
+++ demo-cicd/src/main/java/com/demo/cicd/controller/TestController.java
@@ -3,12 +3,17 @@ package com.demo.cicd.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.cicd.model.Book;
 import com.demo.cicd.repo.BookRepo;
+
+import org.springframework.http.ResponseEntity;
+
 
 @RestController
 public class TestController {
@@ -18,20 +23,20 @@ public class TestController {
 	
 	
 	@GetMapping("/test")
-	public String getBooks() {
-		List<Book> book = bookRepo.findAll();
-		return book.toString();
+	public ResponseEntity<List<Book>> getBooks() {
+		List<Book> books = bookRepo.findAll();
+		return new ResponseEntity<>(books, HttpStatus.OK);
 	}
 	
 	@PostMapping("/test")
-	public String postBooks() {
+	public ResponseEntity<Book>  postBooks() {
 		Book book = new Book();
 		book.setName("The book");
 		book.setAuther("Shivank singh");
 		book.setPages(23);
 		book.setRating((float) 4);
 		book = bookRepo.save(book);
-		return book.toString();
+		return new ResponseEntity<>(book, HttpStatus.OK);
 	}
 
 }
